6f359f3b7c7f430946dc5c45ea9f81fe202da448
Xin Wang <xwang@cita.utoronto.ca>
2015-11-15 14:58:34 -0500

    update

--- src/python/fourier/psxi.py
+++ src/python/fourier/psxi.py
@@ -12,7 +12,7 @@ def pk(d, boxsize=1000.):
     # ->> obtain the angular averaged power spectrum <<- #
 
     # ->> FFT first <<- #
-    dk = sfft.rfftn(d)
+    dk = np.fft.rfftn(d)
     s = d.shape
     sk = dk.shape
 
@@ -27,11 +27,19 @@ def pk(d, boxsize=1000.):
     kx, ky, kz=np.mgrid[0:ng, 0:ng, 0:ng/2+1].astype(float)
     ki = 2.*np.array([np.sin(kx*kmin/2.), np.sin(ky*kmin/2.), np.sin(kz*kmin/2.)])
     k2=4.*(np.sin(kx*kmin/2.)**2.+np.sin(ky*kmin/2.)**2.+np.sin(kz*kmin/2.)**2.)
+    k=np.sqrt(k2)
 
-    print k2.shape
+    print k2.shape, k.shape
 
-    quit()  
 
+    # ->> k bins <<- #
+    bin2f=1/16.
+    bedges=kmin*2.*np.arange(-bin2f/2., np.log(k[-1]/kmin)/np.log(2.), bin2f )
+
+
+    #->> average over given k amplitude <<- #
+    for i in range(len(bedges)-1):
+        #->> 
 
 
     return
