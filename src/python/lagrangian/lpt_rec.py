ad70e35d5479f34677081645d45633bca165cf63
Xin Wang <xwang@cita.utoronto.ca>
2015-11-22 17:27:33 -0500

    update

--- src/python/lagrangian/lpt_rec.py
+++ src/python/lagrangian/lpt_rec.py
@@ -65,12 +65,17 @@ def get_ZA_displacement(p, m, smooth_R=None, smooth_type=None):
 def shifted_ZA(p, si):
     ''' ->> shift uniform grid after obtaining '''
 
-    lgrid=np.linspace(0, p.boxsize, p.nbin, endpoint=False)
-    print 'lgrid shape:', lgrid.shape
+    _do_linear_grid_ = False
 
-    # ->>  <<- #
-    grid=mar.meshgrid(lgrid, lgrid, lgrid)[::-1,...]
-    print 'meshgrid shape:', grid.shape, si.shape
+    if _do_linear_grid_:
+        lgrid=np.linspace(0, p.boxsize, p.nbin, endpoint=False)
+        grid=mar.meshgrid(lgrid, lgrid, lgrid)[::-1,...]
+        print 'lgrid shape:', lgrid.shape, 'meshgrid shape:', grid.shape, si.shape
+    else:
+        _grid_=np.random.uniform(0., p.boxsize, 3*p.nbin)
+
+        grid=mar.meshgrid(_grid_[:p.nbin], _grid_[p.nbin:2*p.nbin], _grid_[2*p.nbin:])[::-1,...]
+        #print 'lgrid shape:', gridx.shape, 'meshgrid shape:', grid.shape, si.shape
 
 
     shifted=np.zeros((3, p.nbin, p.nbin, p.nbin))
@@ -129,34 +134,8 @@ def displaced_ZA(p, si, mpart):
         print 'si shape', si.shape, 'particle pos shape:', mpart.shape
         displaced=pmv.particle_move(p, mpart, si)
 
-
-        """
-        # ->> setup interpolation of si <<- #
-        x_= np.linspace(0., p.boxsize, p.nbin, endpoint=False)
-        y_= np.linspace(0., p.boxsize, p.nbin, endpoint=False)
-        z_= np.linspace(0., p.boxsize, p.nbin, endpoint=False)
-    
-        x, y, z=mar.meshgrid(x_, y_, z_)
-        data =[np.array([x, y, z, si[i]]) for i in range(3)]
-    
-        print 'interpolation data shape:', x_.shape, x.shape, data[0].shape
-    
-        sitp=[]
-        for i in range(3):
-            sitp.append(itp.griddata_interpolater(data[i], 3, method='linear', fill_value=0))
-    
-    
-        # ->> move particles <<- #
-        for i in range(3):
-            dis_=mpart[i]+sitp[i]( (mpart[0], mpart[1], mpart[2]) )
-
-            # ->> periodic boundary <<- #
-	    dis_[np.where(dis_<0.)] = p.boxsize+dis_[np.where(dis_<0.)]
-	    dis_[np.where(dis_>p.boxsize)] = dis_[np.where(dis_>p.boxsize)]-p.boxsize
-	    displaced[i] = np.copy(dis_)
-
-    	    print 'axis-', i, 'is done.'
-        """
+        displaced[np.where(displaced<0.)] = p.boxsize+displaced[np.where(displaced<0.)]
+        displaced[np.where(displaced>p.boxsize)] = displaced[np.where(displaced>p.boxsize)]-p.boxsize
 
     return displaced
 
@@ -193,15 +172,20 @@ def lag_rec_ZA(p, mpart, dmap, smooth_R=None, smooth_type=None, rect_type='ZA_di
 
         _test_draw_ = True
 	if _test_draw_:
-            nplt, ncol = 2, 2
+            shift_=pos_shift.reshape(p.nbin,p.nbin,p.nbin, 3)
+
+            nplt, ncol = 4, 2
             fig,ax=mpl.mysubplots(nplt,ncol_max=ncol,subp_size=7.,gap_size=1,return_figure=True)
 
+
             ax[0].imshow(d_shift[:,:,100])
-	    ax[1].imshow(1.+dmap[:,:,100], norm=colors.LogNorm())
+            ax[1].plot(shift_[:,:,100,1], shift_[:,:,100, 2], 'k.')
+
+	    ax[2].imshow(1.+d_disp[:,:,100], norm=colors.LogNorm())
+	    ax[3].imshow(1.+dmap[:,:,100], norm=colors.LogNorm())
+
 
 	    pl.show()
-
-	    #quit()
 
 
 
@@ -226,3 +210,34 @@ def lag_rec_ZA(p, mpart, dmap, smooth_R=None, smooth_type=None, rect_type='ZA_di
 
 
 
+
+
+
+
+        """
+        # ->> setup interpolation of si <<- #
+        x_= np.linspace(0., p.boxsize, p.nbin, endpoint=False)
+        y_= np.linspace(0., p.boxsize, p.nbin, endpoint=False)
+        z_= np.linspace(0., p.boxsize, p.nbin, endpoint=False)
+    
+        x, y, z=mar.meshgrid(x_, y_, z_)
+        data =[np.array([x, y, z, si[i]]) for i in range(3)]
+    
+        print 'interpolation data shape:', x_.shape, x.shape, data[0].shape
+    
+        sitp=[]
+        for i in range(3):
+            sitp.append(itp.griddata_interpolater(data[i], 3, method='linear', fill_value=0))
+    
+    
+        # ->> move particles <<- #
+        for i in range(3):
+            dis_=mpart[i]+sitp[i]( (mpart[0], mpart[1], mpart[2]) )
+
+            # ->> periodic boundary <<- #
+	    dis_[np.where(dis_<0.)] = p.boxsize+dis_[np.where(dis_<0.)]
+	    dis_[np.where(dis_>p.boxsize)] = dis_[np.where(dis_>p.boxsize)]-p.boxsize
+	    displaced[i] = np.copy(dis_)
+
+    	    print 'axis-', i, 'is done.'
+        """
