b899c6a6c547692efe0dd3e40d830842c4f63be1
Xin Wang <xwang@cita.utoronto.ca>
2015-12-13 20:00:31 -0500

    update

---
+++ src/c/forward_displacement.c
@@ -0,0 +1,88 @@
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <math.h>
+  #include <string.h>
+  #include <fftw3.h>
+
+  #include "const.h"
+  #include "varb.h"
+  #include "mymath.h"
+  #include "myerr.h"
+  #include "matrix.h"
+  #include "init.h"
+  #include "power.h"
+  #include "cospara.h"
+  #include "myinterpolate.h"
+
+  #include "parvar.h"
+  #include "io.h"
+  #include "cic.h"
+  #include "misc.h"
+  #include "poisson.h"
+  #include "fourier.h"
+  #include "backward_displacement.h"
+
+
+  #ifdef _OMP_
+  #include <omp.h>
+  #endif
+
+
+
+
+
+
+
+
+
+
+
+
+/* ->>  Forward modeling of displacement field <<- */
+void displacement_forward(SimInfo *s, float *d, float *disp, int ) {
+  /* ->> obtain ZA+2LPT displacement field from density field <<- */ 
+
+  int fft_return_type;
+  long long ip, i, j, ngrid_tot;
+  float *d2lpt, *phi1, *phi1_i, *phi1_ij, *phi, *phi_i, *phi_ij;
+  //char *other_req="";
+
+  ngrid_tot=s->ngrid_xyz[0]*s->ngrid_xyz[1]*s->ngrid_xyz[2];
+
+  // ->> solve FFTW  to get 1st order phi and phi_i <<- //
+  fft_return_type=_RETURN_HESSIAN_;
+  phi1_ij=(float *)fftwf_malloc(sizeof(float)*ngrid_tot*9);
+
+  poisson_solver_float(d, phi1, phi1_i, phi1_ij, s->boxsize, s->ngrid, s->smooth_type_flag, s->smooth_R, fft_return_type);
+
+  // ->> phi^(2) <<- //
+  phi=(float *)fftwf_malloc(sizeof(float)*ngrid_tot);
+  // ->> smooth density field first <<- //
+  smooth_field(d, s->boxsize, s->ngrid, s->smooth_type_flag, s->smooth_R);
+
+  #ifdef _OMP_
+  #pragma omp parallel for private(ip, i, j)
+  #endif
+  for(ip=0; ip<ngrid_tot; ip++) {
+
+    for(i=0; i<3; i++)
+      for(j=0; j>i; j++) {
+        d[ip] += 3./7.*(ArrayAccess3D_n3(phi1_ij, 3, 3, ngrid_tot, i, i, ip)
+	           * ArrayAccess3D_n3(phi1_ij, 3, 3, ngrid_tot, j, j, ip) 
+                   -pow(ArrayAccess3D_n3(phi1_ij, 3, 3, ngrid_tot, i, j, ip), 2));
+        }
+    }
+
+  //->> solve Poisson equation again <<- //
+  fft_return_type=_RETURN_GRADIENT_;
+
+  poisson_solver_float(d, phi, disp, phi_ij, s->boxsize, s->ngrid, s->smooth_type_flag, 0., fft_return_type);
+
+
+  return;
+  }
+
+
+
+
+
--- src/c/reconstruction_partmoving.c
+++ src/c/reconstruction_partmoving.c
@@ -44,45 +44,56 @@ void reconstruction_partmover(RectCtrl *rc, SimInfo *s, Pdata_pos *p, float *d,
     {do_disp=TRUE;  do_shift=TRUE;  do_disp_shift=TRUE;}
   else {abort();}
 
-  // ->> forward vs. backward displacement <<- //
-  if(strcmp(rc->displacement_type, "backward_displacement")!=0) {
-    printf("only support backward modeling now.\n");
-    fflush(stdout); abort();
-    }
-
-  // ->> LPT order <<- //
-  if(strcmp(rc->displacement_order, "1LPT")==0) 
-    {lpt_order=1;}
-  else if(strcmp(rc->displacement_order, "2LPT")==0) 
-    {lpt_order=2;}
-  else {lpt_order=-1; abort();}
 
 
   /* ->> obtain the displacement field from density field <<- */
   // ->> memory allocation first <<- //
   disp=(float *)fftwf_malloc(sizeof(float)*s->ngrid*s->ngrid*s->ngrid*3);
 
-  if(lpt_order==1){
-    // ->> Zel'dovich Approximation <<- //
+  // ->> forward displacement <<- //
+  if(strcmp(rc->displacement_type, "forward_displacement")==0) {
+    printf("only support backward modeling now.\n");
+    fflush(stdout); abort();
+    }
+  // ->> backward modeling of displacement <<- //
+  else if(strcmp(rc->displacement_type, "backward_displacement")==0) {
 
-    if(rc->do_disp_perturb==TRUE) {
-      printf("perform perturbed-ZA reconstruction.\n"); fflush(stdout);
-      za_displacement_pert(s, d, disp); 
+    // ->> LPT order <<- //
+    if(strcmp(rc->displacement_order, "1LPT")==0) 
+      {lpt_order=1;}
+    else if(strcmp(rc->displacement_order, "2LPT")==0) 
+      {lpt_order=2;}
+    else {lpt_order=-1; abort();}
+
+    if(lpt_order==1){
+      // ->> Zel'dovich Approximation <<- //
+      if(rc->do_disp_perturb==TRUE) {
+        printf("perform perturbed-ZA reconstruction.\n"); fflush(stdout);
+        za_displacement_pert(s, d, disp); 
+        }
+      else{
+        printf("perform unperturbed-ZA reconstruction.\n"); fflush(stdout);
+        za_displacement(s, d, disp); 
+        }
       }
-    else{
-      printf("perform unperturbed-ZA reconstruction.\n"); fflush(stdout);
-      za_displacement(s, d, disp); 
+    else if(lpt_order==2) {
+      // ->> 2-LPT <<- //
+      if(rc->do_disp_perturb==TRUE) {
+        printf("`perturbative' 2LPT NOT supported yet.");
+        fflush(stdout); abort();
+        }
+      else
+        displacement_2lpt(s, d, disp); 
       }
+
+      }
+
+  else {
+    printf("Displacement modeling error.\n");
+    fflush(stdout); abort();
     }
-  else if(lpt_order==2) {
-    // ->> 2-LPT <<- //
-    if(rc->do_disp_perturb==TRUE) {
-      printf("`perturbative' 2LPT NOT supported yet.");
-      fflush(stdout); abort();
-      }
-    else
-      displacement_2lpt(s, d, disp); 
-    }
+
+
 
 
   /* ->> moving particles <<- */
