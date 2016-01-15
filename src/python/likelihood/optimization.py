e335247a73a0f14cf02e6d742eac28ef4ba8256d
Xin Wang <harrygun.wangxin@gmail.com>
2016-01-15 17:33:36 -0500

    update

---
+++ src/python/likelihood/optimization.py
@@ -0,0 +1,19 @@
+import matplotlib
+import matplotlib.pyplot as pl
+import matplotlib.colors as colors
+
+import os
+import numpy as np
+import pynbody as pn
+
+import genscript.progcontrol as pc
+from genscript.extendclass import *
+import genscript.mpiutil as mpi
+import cosmology.power  as power
+import genscript.read as read
+import genscript.myplot as mpl
+import genscript.read as rd
+
+
+
+
