30210df35f3499a4cd21e044d38b75dfbe5877e6
Xin Wang <harrygun.wangxin@gmail.com>
2016-01-20 22:22:35 -0500

    update

---
+++ src/c/test_likelihood.h
@@ -0,0 +1,7 @@
+#ifndef _H_TEST_LIKELIHOOD_
+#define _H_TEST_LIKELIHOOD_
+
+
+void test_displacement(SimInfo *s, Pdata_pos *p, float *d, char *fname_part_init, 
+                       char *fname_out);
+#endif
